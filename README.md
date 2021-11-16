d8946d1a89c0c076a4a91bdbdc6f9f7b15260b06
DeusArtifex <deus3artifex@gmail.com>
2021-11-16 18:49:15 +0100

    Add more funcionality
    More funcionality added to the script.
    --d for picking the output directory
    --f for forcing the overwrite
    README.md updated accordingly.
    
    Also added .gitignore for the folders generated by the script itself.

--- README.md
+++ README.md
@@ -3,8 +3,7 @@ Very generic json2html converter
 
 ## Required to run
 To run this, you'll need json2html on your machine.
-
-It can be installed via pip:
+</br>It can be installed via pip:
 
 	pip install json2html
 				
@@ -14,6 +13,26 @@ Or from the project website: [json2html](https://pypi.org/project/json2html/ "js
 
 	convert_json_to_html.py [path_to_json_file]
 
-As for now, the script converts your json file to html with the same name as the .json provided to the script file. Output file is generated in the same directory as the script. Both these things may may change in the future and if so, README will be updated.
+### Current default behavior
+With no parameters present, script will convert the provided .json into .html file with the same name.
+Output file will be stored in the /output/ folder, which will be created in scripts directory. If there will be a file
+with the same name as the generated one, new file will be generated with incrementation after its name.
+
+### Additional arguments
+
+####--d directory
+
+    convert_json_to_html.py --d "directory_path" [path_to_json_file]
+Use to specify a directory in which the output file should be placed in.
+
+####--f force
+
+    convert_json_to_html.py --f [path_to_json_file]
+Use to force overwrite of the output file. With "--f" present, output file will be generated with the default/chosen
+name, even if it means it will overwrite another file. 
 
 Have a pleasant day.
+
+### [GitHub page](https://github.com/DeusArtifex/convert_json_to_html "convert_json_to_html")
+
+_Created by Rafał "DeusArfifex" Kurek_
--- convert_json_to_html.py
+++ convert_json_to_html.py
@@ -5,7 +5,7 @@ import json
 import argparse
 from json2html import *
 
-
+''' I know it shouldn't be done, but I just wanna
 def install_package(package):
     try:
         return __import__(package, fromlist=['*'])
@@ -14,6 +14,16 @@ def install_package(package):
         pip.main(['install', package])
         print("Installation complete")
         return __import__(package, fromlist=['*'])
+'''
+
+
+def prepare_directory(dir_name = ""):
+    cwd = dir_name
+    if os.path.isdir(cwd):
+        return cwd
+    else:
+        os.mkdir(cwd)
+        return cwd
 
 
 def load_json(json_to_load):
@@ -21,23 +31,40 @@ def load_json(json_to_load):
         loaded_json = json.load(json_file)
     return loaded_json
 
-def generate_name(json_name):
+
+def generate_name(json_name, output_dir, force):
     match = re.match(r"(.*).json", json_name)
+    count = 1
     html_name = match.group(1) + ".html"
-    return html_name
+    while(True):
+        if html_name in os.listdir(output_dir) and force == False:
+            html_name = match.group(1) + str(count) + ".html"
+            count +=1
+        else:
+            return html_name
 
 
-converter = install_package("json2html")
+def parse_args():
+    parser = argparse.ArgumentParser(description='Convert provided json into html file')
+    parser.add_argument(dest='json', action='store', help='Path to json file which you want to convert')
+    parser.add_argument('--d', dest='dir', action='store', default='output',
+                        help='Directory where html file should be generated')
+    parser.add_argument('--f', dest='force', action='store_true', help='Forces overwrite of the generated file')
 
-parser = argparse.ArgumentParser(description='Convert provided json into html file')
-parser.add_argument(dest='json', action='store', help='Path to json file which you want to convert')
+    args = parser.parse_args()
 
-args = parser.parse_args()
+    return args
 
+#converter = install_package("json2html")
+
+
+args = parse_args()
+
+cwd = prepare_directory(args.dir)
 json_to_convert = load_json(args.json)
 json_name = os.path.basename(args.json)
-html_name = generate_name(json_name)
-cwd = os.getcwd()
+html_name = generate_name(json_name, cwd, args.force)
+
 
 with open(os.path.join(cwd, html_name), "w", encoding="utf-8") as html_file:
     print("Saving tool output to: %s" % cwd + "\\" + html_name)
